867590f0114224f4f6a55408caa52568e1cae139
chaeng16 <162279729+chaeng16@users.noreply.github.com>
2024-03-18 23:29:39 +0900

    Create 1161.java

---
+++ 1161.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        
+
+        if(a%2==0){
+            if(b%2==0)
+                System.out.println("짝수+짝수=짝수");
+        
+            else
+                System.out.println("짝수+홀수=홀수");
+        }
+        
+        else{
+            if(b % 2==0)
+                System.out.println("홀수+짝수=홀수");
+    
+            else
+                System.out.println("홀수+홀수=짝수");
+            }
+
+        }
+         
+}
