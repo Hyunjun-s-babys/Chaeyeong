696f3848db3036c4b4fe80b079dbde0079519a0a
chaeng16 <162279729+chaeng16@users.noreply.github.com>
2024-03-19 15:49:03 +0900

    Create 1224.java

---
+++ 1224.java
@@ -0,0 +1,8 @@
+ float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        float d = sc.nextFloat();
+
+        System.out.println(a / b != c / d ? a / b > c / d ? ">" : "<" : "=");
+    }
+}
