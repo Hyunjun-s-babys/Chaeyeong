d6231b350edfe4ceaaa69e2c5689805ba289eb60
chaeng16 <162279729+chaeng16@users.noreply.github.com>
2024-03-28 22:44:10 +0900

    Update 1731.java

--- 1731.java
+++ 1731.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("special characters\n[\\n,\\\",\\\\] is very importan.");
+        System.out.println("special characters\n[\\n,\\\",\\\\] is very important.");
     }
 }
