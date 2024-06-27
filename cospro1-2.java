fc4175069e4e6c44c83d5dbc3961487ec6746391
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-06-27 23:17:30 +0900

    Create cospro1-2.java

---
+++ cospro1-2.java
@@ -0,0 +1,34 @@
+// You may use import as below.
+import java.util.*;
+
+class Solution {
+    public int solution(int price, String grade) {
+        // Write code here.
+        int answer = 0;
+        if(grade.equals("S"))
+            answer=(int)(price - (price*0.05));
+        if(grade.equals("G"))
+            answer=(int)(price - (price*0.1));
+        if(grade.equals("V"))
+            answer=(int)(price - (price*0.15));
+        return answer;
+    }    
+
+    // The following is main method to output testcase.
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int price1 = 2500;
+        String grade1 = new String("V");
+        int ret1 = sol.solution(price1, grade1);
+        
+        // Press Run button to receive output.
+        System.out.println("Solution: return value of the method is " + ret1 + " .");
+        
+        int price2 = 96900;
+        String grade2 = new String("S");
+        int ret2 = sol.solution(price2, grade2);
+        
+        // Press Run button to receive output.
+        System.out.println("Solution: return value of the method is " + ret2 + " .");
+    }
+}
