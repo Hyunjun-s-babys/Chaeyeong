ecd183600feec68fdaa10d13d40f3ae0c15db1ea
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-11 00:00:00 +0900

    Update cospro1-8.java

--- cospro1-8.java
+++ cospro1-8.java
@@ -9,7 +9,7 @@ class Solution {
         }
         int len = str.length();
         for(int i = 0; i < len / 2; i++){
-            if(str.charAt(i) != str.charAt(len - 1 - i)) return false;
+            if(str.charAt(i) != str.charAt(len - 1 - i)) return false
         }
         return true;
     }
