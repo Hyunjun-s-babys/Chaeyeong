0a95ef788b02d955cf820ea192bd7ad8ccb73874
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-17 19:10:46 +0900

    Update cospro6-1.java

--- cospro6-1.java
+++ cospro6-1.java
@@ -9,8 +9,7 @@ class Solution {
 		return answer;
 	}
 
-    // The following is main method to output testcase. The main method is correct and you shall correct solution method.
-    public static void main(String[] args) {
+   
 		Solution sol = new Solution();
 		int[] temperature = {3, 2, 1, 5, 4, 3, 3, 6};
 		int A = 1;
