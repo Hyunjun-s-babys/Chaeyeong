89b6a1dbec37ef09a617d980a57aac9f370ca72e
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-18 21:16:09 +0900

    Create cospro2-2.java

---
+++ cospro2-2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public int func_a(int[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length; ++i)
+            if (arr[i] % 5 == 0)
+                count += 1;
+        return count;
+    }
+    public String func_b(int three, int five){
+        if (three > five)
+            return "three";
+        else if (three < five)
+            return "five";
+        else
+            return "same";
+    }
+    
+    public int func_c(int[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length; ++i)
+            if (arr[i] % 3 == 0)
+                count += 1;
+        return count;
+    }
+    public String solution(int[] arr) {
+        int countThree = func_c(arr);
+        int countFive = func_a(arr);
+        String answer = func_b(countThree,countFive);
+        return answer;
+    }
+   
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {2, 3, 6, 9, 12, 15, 10, 20, 22, 25};
+        String ret = sol.solution(arr);
+
+        
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
