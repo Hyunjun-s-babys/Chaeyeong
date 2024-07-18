9d99baf5fb932fc33482b236d42df30696b605cf
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-19 01:02:03 +0900

    Create cospro2-7.java

---
+++ cospro2-7.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int value, String unit) {
+        
+        int converted = 0;
+        if (unit.equals("C"))
+            converted = (int)(value * 1.8 + 32);
+        if (unit.equals("F"))
+            converted = (int)((value - 32) / 1.8);
+        return converted;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int value = 527;
+        String unit = "C";
+        int ret = sol.solution(value, unit);
+
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+			value = 980;
+			unit = "F";
+			ret = sol.solution(value,unit);
+			System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
