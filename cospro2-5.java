97abb8b9fe9ed21f9df3b35866fe4e18ced4cb06
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-19 01:00:04 +0900

    Create cospro2-5.java

---
+++ cospro2-5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int attack, int recovery, int hp) {
+        int count = 0;
+        while(true){
+            count += 1;
+            hp -= attack;
+            if(hp <= 0)
+                break;
+            hp += recovery;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int attack = 30;
+        int recovery = 10;
+        int hp = 60;
+        int ret = sol.solution(attack, recovery, hp);
+
+        
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
