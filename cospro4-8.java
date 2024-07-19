d022459bb8335c311236d390867ab4a5e6b8d324
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-19 19:57:53 +0900

    Create cospro4-8.java

---
+++ cospro4-8.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class Solution{
+    public int solution(int n, int[] votes) {
+        
+        int[] arr = new int[n+1];
+        for(int i=0; i<votes.length; i++) {
+            arr[votes[i]]++;
+        }
+        for(int i=1; i<n+1; i++)
+            if(arr[i] > votes.length/2)
+                return i;
+        return -1;
+    }    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n1 = 3;
+        int[] votes1 = {1, 2, 1, 3, 1, 2, 1};
+        int ret1 = sol.solution(n1, votes1);
+
+        
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+        
+        int n2 = 2;
+        int[] votes2 = {2, 1, 2, 1, 2, 2, 1};
+        int ret2 = sol.solution(n2, votes2);
+
+        
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");        
+    }    
+}
