9cc6432bb90a6ff42dbba935045ef84b3a8fd306
Minchaeyeong <162279729+chaeng16@users.noreply.github.com>
2024-07-19 19:53:38 +0900

    Create cospro4-4.java

---
+++ cospro4-4.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+class Solution{
+    public int solution(int[] classes, int m) {
+        int answer = 0;
+        for(int i=0; i<classes.length; i++) {
+            answer += classes[i] / m;
+            if (classes[i] % m != 0)
+                answer++;
+        }
+        return answer;
+    }    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] classes = {80, 45, 33, 20};
+        int m = 30;
+        int ret = sol.solution(classes, m);
+        
+        
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
+       
+ 
